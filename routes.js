2ed9c761ead9041b2542444919b44be0023428d0
Todd Vanderlin <you@example.com>
2017-07-26 18:53:42 -0400

    acc auth change

--- routes.js
+++ routes.js
@@ -11,12 +11,13 @@ admin.initializeApp({
 module.exports = function(app) {
 	
 	var spotifyRef = function(req) {
-		var redirectURL = req.protocol + '://' + req.get('host');
+		var redirectURL = process.env.SPOTIFY_REDIRECT_URI
+		// req.protocol + '://' + req.get('host') || 
+		console.log('redirectURL', redirectURL);
 		return new SpotifyWebApi({
 			clientId : process.env.SPOTIFY_CLIENT_ID,
 			clientSecret : process.env.SPOTIFY_CLIENT_SECRET,
-			redirectUri : redirectURL+'/callback/'
-			//process.env.SPOTIFY_REDIRECT_URI
+			redirectUri : redirectURL
 		});
 	}
 
